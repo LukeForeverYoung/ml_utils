f9df2765dc2aaf6a1bd9dbecd110937a5e3fa010
Luke <mizukiluke@outlook.com>
2019-10-04 18:16:50 +0800

    add Time cost tool

--- TimeUtils.py
+++ TimeUtils.py
@@ -7,17 +7,25 @@ class TimeCoster():
     def start(self):
         self.__time_tick = time.time()
 
-    def tock(self):
+    def tock(self,print_s=False):
         time_cost=time.time() - self.__time_tick
         self.__time_tick=time.time()
+        if print_s:
+            self.print_time(time_cost)
         return time_cost
 
-    def cost_from_start(self):
-        return time.time() - self.__time_start
+    def cost_from_start(self,print_s=False):
+        time_cost=time.time() - self.__time_start
+        if print_s:
+            self.print_time(time_cost)
+        return time_cost
+
+    def print_time(self,s):
+        print('cost {:.2g} s'.format(s))
 if __name__ == '__main__':
     coster=TimeCoster()
     time.sleep(5)
-    print(coster.tock())
+    coster.tock(print_s=True)
     time.sleep(5)
-    print(coster.tock())
-    print(coster.cost_from_start())
+    coster.tock(print_s=True)
+    coster.cost_from_start(print_s=True)
